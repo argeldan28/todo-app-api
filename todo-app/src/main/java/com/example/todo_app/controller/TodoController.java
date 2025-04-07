403e9c07649d9d179f15532c692731cf9446e6c7
Argel <argeldan28@gmail.com>
2025-04-07 21:19:23 +0200

    first commit

---
+++ todo-app/src/main/java/com/example/todo_app/controller/TodoController.java
@@ -0,0 +1,62 @@
+package com.example.todo_app.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.todo_app.model.Todo;
+import com.example.todo_app.repository.TodoRepository;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    //@Autowired
+    private final TodoRepository repository;
+
+    public TodoController(TodoRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Todo getTodoById(@PathVariable Integer id) {
+       return repository.findById(id)
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Todo not found"));
+    }
+    
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo){
+        return repository.save(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Integer id, @RequestBody Todo updatedTodo) {
+        return repository.findById(id).map(t -> {
+            t.setTitle(updatedTodo.getTitle());
+            t.setCompleted(updatedTodo.isCompleted());
+            return repository.save(t);  
+        }).orElseThrow();
+        
+    }
+
+    public void deleteTodo(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
+}
